fb1c8d5752298518fa198150191b3d7452c33687
kyle Dornan <kyledornan26@gmail.com>
2020-06-04 13:52:41 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+
+class Dog
+  def dog_name (name)
+    name = "fido"
+    name = "snoop"
+    name = "lassie"
+
+end 
+ end  
+ class Person 
+ end 
+ adele_goldberg = Person.new 
+ alan_kay = Person.new 
